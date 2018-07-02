548204e2202e961bcb3dbc7c5aa290eb5f184b5c
DANNY <36015978+Danny-AZP@users.noreply.github.com>
2018-07-01 22:57:23 -0500

    Add files via upload

---
+++ ITipoAereo.cs
@@ -0,0 +1,13 @@
+public interface ITipoAereo <ta>
+{
+  new display()
+  {
+    ita=Volador();
+    ita=Atletico();
+    ita=new Agil
+       saltar();
+       maniobrar();
+       golpear();
+       aplicar();
+  }
+}
---
+++ PersonajeAnimado.cs
@@ -0,0 +1,16 @@
+using system;
+Public class Luchador:ITipoAereo
+{
+new display(){
+ new comport ITipoAereo = newVolador();
+{
+    ita=Volador();
+    ita=Atletico();
+    ita=new Agil
+    saltar();
+       maniobrar();
+       golpear();
+       aplicar();
+}  
+}
+}
